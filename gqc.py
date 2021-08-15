a3617f25517bd4d4488c5ceaad68eb4d2832aebf
Jim Hanlon <jim@hanlonsoftware.com>
2021-08-15 16:39:41 -0400

    Refactor Validate from outer class to separate module

--- gqc.py
+++ gqc.py
@@ -3,6 +3,7 @@
 from cache import Cache
 from canonicalize import Canonicalize
 from util import Util
+from validate import Validate
 
 import configparser
 import csv
@@ -189,46 +190,6 @@ class Geometry:
 
     def canonicalize_latlon(self, latlon):
         return float('{0:.{1}f}'.format(float(latlon),self.precision))
-
-
-class Validate:
-    @staticmethod
-    def dir_writable(dnm):
-        if os.path.exists(dnm):
-            # path exists
-            if os.path.isdir(dnm): # is it a file or a dir?
-                # also works when dir is a link and the target is writable
-                return os.access(dnm, os.W_OK)
-            else:
-                return False # path is a file, so cannot write as a dir
-        # target does not exist, check perms on parent dir
-        pdir = os.path.dirname(dnm)
-        if not pdir: pdir = '.'
-        # target is creatable if parent dir is writable
-        return os.access(pdir, os.W_OK)
-
-
-    @staticmethod
-    def file_readable(filename):
-        if os.path.exists(filename) and os.path.isfile(filename):
-            return os.access(filename, os.R_OK)
-        return False
-
-
-    @staticmethod
-    def file_writable(filename):
-        if os.path.exists(filename):
-            # path exists
-            if os.path.isfile(filename): # is it a file or a dir?
-                # also works when file is a link and the target is writable
-                return os.access(filename, os.W_OK)
-            else:
-                return False # path is a dir, so cannot write as a file
-        # target does not exist, check perms on parent dir
-        pdir = os.path.dirname(filename)
-        if not pdir: pdir = '.'
-        # target is creatable if parent dir is writable
-        return os.access(pdir, os.W_OK)
 
 
 class GQC:
---
+++ validate.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import os
+
+class Validate:
+    @staticmethod
+    def dir_writable(dnm: str) -> bool:
+        if os.path.exists(dnm):
+            # path exists
+            if os.path.isdir(dnm): # is it a file or a dir?
+                # also works when dir is a link and the target is writable
+                return os.access(dnm, os.W_OK)
+            else:
+                return False # path is a file, so cannot write as a dir
+        # target does not exist, check perms on parent dir
+        pdir = os.path.dirname(dnm)
+        if not pdir: pdir = '.'
+        # target is creatable if parent dir is writable
+        return os.access(pdir, os.W_OK)
+
+
+    @staticmethod
+    def file_readable(filename: str) -> bool:
+        if os.path.exists(filename) and os.path.isfile(filename):
+            return os.access(filename, os.R_OK)
+        return False
+
+
+    @staticmethod
+    def file_writable(filename: str) -> bool:
+        if os.path.exists(filename):
+            # path exists
+            if os.path.isfile(filename): # is it a file or a dir?
+                # also works when file is a link and the target is writable
+                return os.access(filename, os.W_OK)
+            else:
+                return False # path is a dir, so cannot write as a file
+        # target does not exist, check perms on parent dir
+        pdir = os.path.dirname(filename)
+        if not pdir: pdir = '.'
+        # target is creatable if parent dir is writable
+        return os.access(pdir, os.W_OK)
